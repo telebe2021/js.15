019e759a28ebdf321805912de11da3073d69787c
telebe2021 <mahsungulmmmdov@gmail.com>
2023-12-06 16:10:18 +0400

    commit olundu

--- Script/script.js
+++ Script/script.js
@@ -9,7 +9,7 @@ window.addEventListener("scroll", () => {
     }
 });
 const menu = document.querySelector(".modal-menu");
-document.querySelector("#click").addEventListener('click', (event) => {
+document.querySelector("#icon").addEventListener('click', (event) => {
     event.preventDefault();
     menu.style.display = "block";
     document.querySelector(".modal-bg").style.display = "block";
@@ -29,15 +29,22 @@ document.addEventListener('click', (event) => {
 fetch('http://localhost:3000/robots')
     .then(res => res.json())
     .then(data => {  
+        console.log(data);
         data.forEach(robot => {
-            document.querySelector('.sec22').innerHTML += `
+            document.querySelector('.tion22').innerHTML += `
             <div class="card">
                     <div class="img"><img src="${robot.image}" alt=""></div>
                     <div class="text">
                         <h5>${robot.name}</h5>
                         <p>${robot.description}</p>
                         <a href="../details.html?id=${robot.id}"><button onclick="deleteData(${robot.id})"> VIEW DETAILS</button></a>
-                    </div>
+                        <button onclick="deleteRobot(${robot.id})"> Delete</button>
+                        </div>
                 </div>`
         });
     });
+
+    function deleteRobot(id){
+        axios.delete(`http://localhost:3000/robots/${id}`);
+        Window.location.reload();
+    }
